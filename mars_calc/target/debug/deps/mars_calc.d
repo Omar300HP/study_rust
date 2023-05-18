ecd863c198122d50ec1026a0ca78c92b5a7e1407
Omar Essam <omar.essam.se@gmail.com>
2023-05-18 09:18:11 +0300

    Delete mars_calc/target directory

--- mars_calc/target/debug/deps/mars_calc.d
+++
@@ -1,5 +0,0 @@
-C:\repo\rust\mars_calc\target\debug\deps\mars_calc.exe: src\main.rs
-
-C:\repo\rust\mars_calc\target\debug\deps\mars_calc.d: src\main.rs
-
-src\main.rs:
--- mars_calc/target/debug/mars_calc.d
+++
@@ -1,1 +0,0 @@
-C:\repo\rust\mars_calc\target\debug\mars_calc.exe: C:\repo\rust\mars_calc\src\main.rs
