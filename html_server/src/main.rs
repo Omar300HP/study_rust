64e8830de5a453813087b79bf58ec0dc2333b038
omar300hp <omar.essam.se@gmail.com>
2023-05-18 10:26:43 +0300

    lesson: struct, impl, Strings and &str

--- html_server/src/main.rs
+++ html_server/src/main.rs
@@ -1,3 +1,28 @@
 fn main() {
-    println!("Hello, world!");
+    let server = Server::new(String::from("127.0.0.1"), String::from("8080"));
+
+    server.run();
+
+    println!(
+        "Server is running on ip:{}, port:{}, address:{}",
+        server.ip, server.port, server.address
+    );
+}
+
+struct Server {
+    ip: String,
+    port: String,
+    address: String,
+}
+
+impl Server {
+    fn new(ip: String, port: String) -> Self {
+        Self {
+            address: format!("{}:{}", &ip, &port),
+            ip,
+            port,
+        }
+    }
+
+    fn run(&self) {}
 }
